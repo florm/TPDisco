e8743241ec7e0ee572e3dca058c4ba9ae00ba0bf
AlejandraCasuscelli <alejandracasuscelli@gmail.com>
2017-09-06 17:59:24 -0300

    version tp1 con test

--- src/ar/edu/unlam/pb2/Disco.java
+++ src/ar/edu/unlam/pb2/Disco.java
@@ -20,66 +20,56 @@ public class Disco {
 			this.perimetroExterior= actualizarPerimetroExterior();
 			//para calcular la superficie del disco se resta del area total del disco, el area del circulo interno,
 			//ya que un disco tiene en el centro un area vacia
-			this.superficie= actualizarSuperficie();
-			
+			this.superficie= actualizarSuperficie();	
 		}
 	
 	//metodos get
-	public Double getRadioInterior() {
-		return radioInterior;
-	}
-	
-	public Double getRadioExterior() {
-		return radioExterior;
-	}
-	
-	public Double getPerimetroExterior() {
-		return perimetroExterior;
-	}
-	
-	public Double getPerimetroInterior() {
-		return perimetroInterior;
-	}
-	
-	public Double getSuperficie() {
-		return superficie;
-	}
-	
+		public Double getRadioInterior() {
+			return radioInterior;
+		}
+		
+		public Double getRadioExterior() {
+			return radioExterior;
+		}
+		
+		public Double getPerimetroExterior() {
+			return perimetroExterior;
+		}
+		
+		public Double getPerimetroInterior() {
+			return perimetroInterior;
+		}
+		
+		public Double getSuperficie() {
+			return superficie;
+		}
 	
 	//metodos set
-	
 		public void cambiarRadioInterior(Double radioInterior) {
 			this.radioInterior = radioInterior;
 			this.perimetroInterior=actualizarPerimetroInterior();
-			this.superficie=actualizarSuperficie();
-			
-				
+			this.superficie=actualizarSuperficie();	
 		}
+		
 		public void cambiarRadioExterior(Double radioExterior) {
 			this.radioExterior = radioExterior;
 			this.perimetroInterior=actualizarPerimetroExterior();
 			this.superficie=actualizarSuperficie();
 		}
-	
-	
-	public Double actualizarPerimetroInterior(){
 		
-		perimetroInterior=2 * Math.PI * this.radioInterior;
-		return perimetroInterior;
-	}
-	
-	public Double actualizarPerimetroExterior(){
+		public Double actualizarPerimetroInterior(){
+			perimetroInterior=2 * Math.PI * this.radioInterior;
+			return perimetroInterior;
+		}
 		
-		perimetroExterior=2 * Math.PI * this.radioExterior;
-		return perimetroExterior;
-	}
-	
-	public Double actualizarSuperficie(){
+		public Double actualizarPerimetroExterior(){
+			perimetroExterior=2 * Math.PI * this.radioExterior;
+			return perimetroExterior;
+		}
 		
-		this.superficie= Math.PI * Math.pow(this.radioExterior, 2)-Math.PI * Math.pow(this.radioInterior, 2);
-		return superficie;
-	}
-	
-	
+		public Double actualizarSuperficie(){
+			this.superficie= Math.PI * Math.pow(this.radioExterior, 2)-Math.PI * Math.pow(this.radioInterior, 2);
+			return superficie;
+		}
 	
 }
